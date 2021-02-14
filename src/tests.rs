01a0639be498c93cf4023335b0b2c722d3255e12
Einberg <einbergisak@gmail.com>
2021-02-14 23:27:30 +0100

    forgot to add test file to git...

---
+++ src/tests.rs
@@ -0,0 +1,19 @@
+
+
+
+#[cfg(test)]
+mod tests{
+    use crate::{Coordinate, GameContainer, main};
+
+
+    #[test]
+    fn array_coordinates_are_properly_set(){
+        // Arrange
+        let g = GameContainer::new("asd".to_string());
+        let t = g.tile_array;
+        assert_eq!(t[0][0].coordinates, Coordinate::new(0, 0));
+        assert_eq!(t[5][2].coordinates, Coordinate::new(5, 2));
+        assert_eq!(t[15][15].coordinates, Coordinate::new(15, 15));
+    }
+
+}
