5913b2fcb0f6068c34d770a313199abc0793152d
Einberg <einbergisak@gmail.com>
2021-02-14 21:26:53 +0100

    Adds algorithm for revealing nearby tiles

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,61 @@
+const GAME_COLS: usize = 16;
+const GAME_ROWS: usize = 16;
 
 
-const GAME_WIDTH: usize = 16;
-const GAME_HEIGHT: usize = 16;
-
+struct Coordinate{
+    x: usize,
+    y: usize
+}
 
 struct Tile{
     is_revealed: bool,
     is_bomb: bool,
-    number: u8,
-    coordinates: (usize, usize),
+    number: Option<u8>,
+    coordinates: Coordinate,
 }
 
 struct GameContainer{
-    tile_rows: [[Tile; GAME_WIDTH]; GAME_HEIGHT]
+    tile_array: [[Tile; GAME_ROWS]; GAME_COLS]
 }
 
 impl GameContainer{
-    fn reveal_tile(&mut self, &(x, y): (usize, usize)) {
-        let mut tile = &mut self.tile_rows[y][x];
+    fn reveal_tile(&mut self, coordinate: Coordinate) {
+        let mut tile = &mut self.tile_array[coordinate.x][coordinate.y];
         if !tile.is_revealed{
             if tile.is_bomb{
                 self.lose(&tile);
             }else{
-                tile.is_revealed = true;
-                if tile.number == 0 {
-                    self.reveal_nearby(&tile)
+                self.reveal_nearby(&mut tile)
+            }
+        }
+    }
+
+    fn reveal_nearby(&mut self, mut tile: &mut Tile){
+        if !tile.is_revealed{
+            match tile.number{
+                None if tile.is_bomb => {return}
+                None => {
+                    tile.is_revealed = true;
+                    for xd in -1..=1{
+                        for yd in -1..=1{
+                            // Makes sure that it doesn't go outside of the tile_array index bounds
+                            match (tile.coordinates.x+xd, tile.coordinates.y+yd){
+                                (x, y) if x < 0 || x >= GAME_COLS || y < 0 || y > GAME_ROWS => {}
+                                (x, y) =>{
+                                    self.reveal_nearby(&mut self.tile_array[x][y])
+                                }
+                            }
+                        }
+                    }
+                }
+                Some(_) =>{
+                    tile.is_revealed = true;
                 }
             }
         }
+
+
+
     }
 
 }
