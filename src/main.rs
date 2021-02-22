1e60cf7b6a3c413acb60ff932dcf291fcced1a9a
Einberg <einbergisak@gmail.com>
2021-02-22 23:14:39 +0100

    Filetree update

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,33 @@ use piston_window::*;
 
 fn main() {
 
-    // let mut start_window: PistonWindow = WindowSettings::new("Enter game settings.")
+    let mut start_window: PistonWindow = WindowSettings::new("Enter game settings.", [200,400]).exit_on_esc(true).build().unwrap();
     
-    // while let Some(event) = window.next() {
+    if let Some(event) = start_window.next(){
+        start_window.draw_2d(&event, |context, graphics, _device| {
+            clear([0.2; 4], graphics);
 
-    // }
-    // let mut main_window: PistonWindow =
-    //     WindowSettings::new("Hello Piston!", [800, 800])
-    //     .exit_on_esc(true).build().unwrap();
+        });
+    } else{
+        panic!("Failed to start game. Check that your system is compatible with the Piston library");
+    }
     
-    // while let Some(event) = main_window.next() {
-    //     main_window.draw_2d(&event, |context, graphics, _device| {
-    //         clear([0.2; 4], graphics);
-    //         rectangle([1.0, 0.0, 0.0, 1.0], // red
-    //                   [0.0, 0.0, 100.0, 100.0],
-    //                   context.transform,
-    //                   graphics);
-    //         rectangle([1.0, 1.0, 0.0, 1.0], // red
-    //                   [0.0, 0.0, 50.0, 100.0],
-    //                   context.transform,
-    //                   graphics);
-    //     });
-    // }
+
+    let mut main_window: PistonWindow =
+        WindowSettings::new("Rustsweeper", [800, 800])
+        .exit_on_esc(true).build().unwrap();
+    
+    while let Some(event) = main_window.next() {
+        main_window.draw_2d(&event, |context, graphics, _device| {
+            clear([0.2; 4], graphics);
+            rectangle([1.0, 0.0, 0.0, 1.0], // red
+                      [0.0, 0.0, 100.0, 100.0],
+                      context.transform,
+                      graphics);
+            rectangle([1.0, 1.0, 0.0, 1.0], // red
+                      [0.0, 0.0, 50.0, 100.0],
+                      context.transform,
+                      graphics);
+        });
+    }
 }
