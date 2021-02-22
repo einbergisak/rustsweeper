3e528c91e0f7767730b99e672408ca16b46c4f0a
Einberg <einbergisak@gmail.com>
2021-02-22 22:53:55 +0100

    Implements variable game size, Piston import

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
-use crate::game_container::{GameContainer, GAME_ROWS};
-use crate::tile::{Tile, Coordinate};
+use crate::game::{GameContainer};
 use std::{io::stdin, ops::Add};
 
+extern crate piston_window;
+
 mod tests;
-mod game_container;
-mod tile;
-
-
-
+mod game;
+use piston_window::*;
 
 fn main() {
-    let mut game = GameContainer::new("testt");
 
+    // let mut start_window: PistonWindow = WindowSettings::new("Enter game settings.")
     
+    // while let Some(event) = window.next() {
+
+    // }
+    // let mut main_window: PistonWindow =
+    //     WindowSettings::new("Hello Piston!", [800, 800])
+    //     .exit_on_esc(true).build().unwrap();
     
-    
+    // while let Some(event) = main_window.next() {
+    //     main_window.draw_2d(&event, |context, graphics, _device| {
+    //         clear([0.2; 4], graphics);
+    //         rectangle([1.0, 0.0, 0.0, 1.0], // red
+    //                   [0.0, 0.0, 100.0, 100.0],
+    //                   context.transform,
+    //                   graphics);
+    //         rectangle([1.0, 1.0, 0.0, 1.0], // red
+    //                   [0.0, 0.0, 50.0, 100.0],
+    //                   context.transform,
+    //                   graphics);
+    //     });
+    // }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -3,18 +3,9 @@
 
 #[cfg(test)]
 mod tests{
-    use crate::tile::Coordinate;
-    use crate::game_container::GameContainer;
+    use crate::game::{GameContainer, Coordinate};
 
 
-    #[test]
-    fn array_coordinates_are_properly_set(){
-        // Arrange
-        let g = GameContainer::new("asd");
-        let t = g.tile_array;
-        assert_eq!(t[0][0].coordinates, Coordinate::new(0, 0));
-        assert_eq!(t[5][2].coordinates, Coordinate::new(5, 2));
-        assert_eq!(t[15][15].coordinates, Coordinate::new(15, 15));
-    }
+   
 
 }
